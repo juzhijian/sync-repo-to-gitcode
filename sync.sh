1b45e186c105dd656a9c3cca472544adf615886e
聚指尖 <princergx@live.com>
2021-12-02 23:53:28 +0800

    Update

--- sync.sh
+++ sync.sh
@@ -12,14 +12,14 @@ echo "GITHUB_REPO=$GITHUB_REPO"
 echo "CODING_REPO=$CODING_REPO"
 echo "GETSH=$GETSH"
 
-
-curl -sSO $GETSH && bash main.sh
-
-wget $GETSH
-chmod -R 777 main.sh
-./main.sh
-
 git clone --mirror "$GITHUB_REPO" && cd `basename "$GITHUB_REPO"`
+
+if [-n “$GETSH” ];then
+    wget $GETSH
+    chmod -R 777 main.sh
+    ./main.sh
+fi
+
 git remote set-url --push origin "$CODING_REPO"
 git fetch -p origin
 git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
