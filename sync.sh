81938e1c8033c41644917bb0409034d7db5399e9
聚指尖 <princergx@live.com>
2021-12-02 21:36:04 +0800

    Update

--- sync.sh
+++ sync.sh
@@ -4,11 +4,13 @@ set -e
 
 GITHUB_REPO=$1
 CODING_REPO=$2
+GETSH=$3
 
 GIT_SSH_COMMAND="ssh -v"
 
 echo "GITHUB_REPO=$GITHUB_REPO"
 echo "CODING_REPO=$CODING_REPO"
+echo "GETSH=$GETSH"
 
 git clone --mirror "$GITHUB_REPO" && cd `basename "$GITHUB_REPO"`
 git remote set-url --push origin "$CODING_REPO"
