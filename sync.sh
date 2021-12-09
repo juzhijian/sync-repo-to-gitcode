002f20ee5cc465a894c084d882f8a849c831c705
聚指尖 <admin@juzhijian.com>
2021-12-09 14:32:13 +0800

    Update

--- sync.sh
+++ sync.sh
@@ -25,14 +25,13 @@ if [ -z "$GETSH" ]
         wget $GETSH
         chmod -R 777 main.sh
         ./main.sh
-        echo "替换完成！"
         rm -rf main.sh
         echo "删除替换文件！"
         git add .
         git commit -m '本地化测试'
 fi
 
-echo "替换远程仓库"
+echo "开始替换远程仓库"
 git remote set-url --push origin "$CODING_REPO"
 git fetch -p origin
 git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
